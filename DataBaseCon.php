07447b563b53e4a23b8d60e252a6f01d2eda7d6a
densolomon <solomon4ik@gmail.com>
2015-06-13 13:28:43 +0300

    DataBaseCon add db_connetct

--- DataBaseCon.php
+++ DataBaseCon.php
@@ -1,13 +1,28 @@
 <?php
-    class DataBAseCon {
+    class DataBAseCon
+    {
         static private $_instance = null;
         public $db;
-        
-        private function __construct() {}
 
-        private function __clone() {}
+        private function __construct()
+        {
+        }
 
-        function myfunc($a){
+        private function __clone()
+        {
+        }
+
+        function myfunc($a)
+        {
 
         }
-    }
+}
+$link = mysql_connect('localhost', 'admin', '111');
+if (!$link)
+{
+    die('Ошибка соединения: ' . mysql_error());
+}
+
+else{
+    echo "Соединение установлено!=)";
+}
