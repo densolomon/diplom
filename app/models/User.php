91e4caad3b43e24cd657a48b5f8444370687c7d2
densolomon <solomon4ik@gmail.com>
2015-08-03 19:40:56 +0300

    add $password into user.php

--- app/models/User.php
+++ app/models/User.php
@@ -3,6 +3,7 @@
 class User {
     public $id_user;
     public $name;
+    public $password;
     public $surname;
     public $patronymic;
     public $email;
@@ -10,11 +11,13 @@ class User {
     public $adress;
     public $status;
 
-    public function __construct ($id_user="NULL", $name="NULL", $surname="NULL", $patronymic="NULL",
-                                 $email="NULL", $phone_number="NULL", $adress="NULL", $status="NULL"
+    public function __construct ($id_user="NULL", $name="NULL", $password="NULL", $surname="NULL",
+                                 $patronymic="NULL", $email="NULL", $phone_number="NULL",
+                                 $adress="NULL", $status="NULL"
     ){
         $this->id_user      = ($id_user     =="NULL") ? $this->id_user      : $id_user;
         $this->name         = ($name        =="NULL") ? $this->name         : $name;
+        $this->password     = ($password    =="NULL") ? $this->password     : $password;
         $this->surname      = ($surname     =="NULL") ? $this->surname      : $surname;
         $this->patronymic   = ($patronymic  =="NULL") ? $this->patronymic   : $patronymic;
         $this->email        = ($email       =="NULL") ? $this->email        : $email;
